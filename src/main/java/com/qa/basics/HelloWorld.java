610ce98d6ea45f9bf61c6601ef71e1dcdc7b476a
renuka magar <renumagar905@gmail.com>
2026-09-23 20:27:42 +0530

    initial commit

---
+++ src/main/java/com/qa/basics/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.qa.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="QA Engineer";
+		System.out.println("Hello Im a "+name +" this is my first Project");
+
+	}
+
+}
